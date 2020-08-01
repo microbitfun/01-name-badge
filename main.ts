e6dbc27ce9bf2317cdc3d4de4de33aac20e8fddb
MicrobitFun <68689935+microbitfun@users.noreply.github.com>
2020-08-01 15:38:27 +0200

    cleaned

--- main.ts
+++ main.ts
@@ -1,10 +1,6 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showString("MicrobitFun")
+    basic.showString("Microbitfun")
 })
 input.onButtonPressed(Button.B, function () {
     basic.showNumber(8)
-})
-basic.showIcon(IconNames.Heart)
-basic.forever(function () {
-	
 })
