10936f25fd8b577a434e1efc3f691cc8255ac01e
MicrobitFun <68689935+microbitfun@users.noreply.github.com>
2020-08-12 10:17:02 +0200

    updated code according book screenshot

--- main.ts
+++ main.ts
@@ -1,6 +1,10 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showString("Microbitfun")
+    basic.showString("Invent to learn with the micro:bit")
 })
 input.onButtonPressed(Button.B, function () {
     basic.showNumber(8)
+})
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+	
 })
